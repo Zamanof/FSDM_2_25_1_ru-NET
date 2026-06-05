ff562fb29bde5962a18e0f2047a16bd5e8a6e83f
zamanov <zamanov@STHQ012D-01>
2026-06-05 11:42:34 +0400

    Add project files.

---
+++ NET 01. First Program (Old version)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NET_01._First_Program__Old_version_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
